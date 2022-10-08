c022360a1319fc13113a00d96059fb64b8294943
Nicholas DiPatri <ndipatri@gmail.com>
2022-10-08 11:36:39 -0400

    adding license attribution

--- src/roboGaggia.ino
+++ src/roboGaggia.ino
@@ -1,9 +1,20 @@
-/*
- * Project roboGaggia
- * Description:
- * Author:
- * Date:
- */
+/**
+Copyright [2022] [Nicholas J. DiPatri]
+
+Please give me attribution (keep my name somewhere) if you use this code in part or whole. Thanks!
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**/
 
 #include <Wire.h> 
 #include <string.h> 
@@ -251,7 +262,6 @@ struct GaggiaState {
    boolean  fillingReservoir = false;
    boolean  brewHeaterOn = false;
    boolean  steamHeaterOn = false;
-   boolean  cooling = false;
    boolean  measureTemp = false;
    boolean  tareScale = false;
    boolean  dispenseWater = false;
@@ -484,7 +494,7 @@ void setup() {
   coolingState.display3 =          "                    ";
   coolingState.display4 =          "Click to Stop       ";
   coolingState.dispenseWater = true; 
-  coolingState.cooling = true; 
+  coolingState.measureTemp = true;
 
   coolDoneState.state = COOL_DONE;
   coolDoneState.display1 =         "Discard water.      ";
@@ -909,10 +919,6 @@ void processCurrentGaggiaState(GaggiaState currentGaggiaState,
     if (!waterReservoirState->manualOverride) {
       turnWaterReservoirSolenoidOff();
     }
-  }
-
-  if (currentGaggiaState.cooling) {
-    readHeaterState(MAX6675_CS_brew, MAX6675_SO_brew, MAX6675_SCK, heaterState);  
   }
 
   if (currentGaggiaState.measureTemp) {
