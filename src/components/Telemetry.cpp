da8623fc8035dac97b77d8b4312a0869d8fdf688
Nicholas DiPatri <ndipatri@gmail.com>
2023-03-12 13:06:01 -0400

    media updates.

--- src/components/Telemetry.cpp
+++ src/components/Telemetry.cpp
@@ -18,7 +18,7 @@ void calculateAndSendTelemetryIfNecessary() {
                               String(pressure_PID_kI) + ":" +
                               String(pressure_PID_kD) + ")";
     telemetry.stateName = getStateName(currentGaggiaState.state);
-    telemetry.measuredWeightGrams = scaleState.measuredWeight - scaleState.tareWeight;
+    telemetry.measuredWeightGrams = abs(scaleState.measuredWeight - scaleState.tareWeight);
     telemetry.measuredPressureBars = waterPumpState.measuredPressureInBars;
     telemetry.pumpDutyCycle = waterPumpState.pumpDutyCycle;
     telemetry.flowRateGPS = waterPumpState.flowRateGPS;
